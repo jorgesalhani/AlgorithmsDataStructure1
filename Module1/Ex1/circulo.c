5eaed5c86c95316e8f610b74b3bc52cbf40a9d63
jorgesalhani <jorge.salhani@usp.br>
2022-08-31 14:20:08 -0300

    Get TAD example. Start Ex 1

---
+++ Module1/Ex1/circulo.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "circulo.h"
+
+struct circulo {
+  
+}
+
+CIRCULO *circulo_criar (PONTO *p, float raio) {
+
+}
+
+bool circulo_set_ponto (CIRCULO *c,PONTO *p) {
+
+}
+
+bool circulo_set_raio (CIRCULO *c, int raio) {
+
+}
+
+PONTO *circulo_get_ponto (CIRCULO *c) {
+
+}
+
+int circulo_get_raio (CIRCULO *c) {
+
+}
+
+void circulo_apagar (CIRCULO **circulo) {
+
+}
---
+++ Module1/Ex1/ponto.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ponto.h"
+
+struct ponto_ {
+  float x; 
+  float y;
+}
+
+PONTO *ponto_criar (float x, float y) {
+
+}
+
+void ponto_apagar (PONTO **p) {
+
+}
+
+bool ponto_set (PONTO *p, float x, float y) {
+
+}
+
+float ponto_get_x (PONTO *p) {
+
+}
+
+float ponto_get_y (PONTO *p) {
+
+}
+
+void ponto_print (PONTO *p) {
+
+}
