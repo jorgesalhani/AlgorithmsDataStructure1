bcf81b023a37d9506d0305e0a5d0ba500e02980d
jorgesalhani <jorge.salhani@usp.br>
2022-09-24 01:59:02 -0300

    Comments

--- Module2/Ex3/Pilha.c
+++ Module2/Ex3/Pilha.c
@@ -66,42 +66,55 @@ void pilha_print(PILHA* p) {
   item_imprimir(pilha_topo(p));
 }
 
+
+
 bool balanceada(char *sequencia) {
   char c = sequencia[0];
   int i = 1;
 
+  // Inicializacao da pilha a receber os caracteres
   PILHA *pilha_aberta = pilha_criar();
-
   ITEM *item;
   ITEM *it;
+  bool str_balanceada = true;
+
   while (c != '\0') {
+    
+    // Para cada caracter, caso seja um "caracter de abertura (, [, {", empilhar
     if (c == '(' || c == '[' || c == '{') {
       item = item_criar(c);
       pilha_empilhar(pilha_aberta, item);
     } else {
+
+      // Caso nao seja um caracter de abertura (i.e. carter de fechamento ), ], } ), 
+      // verificar qual o caracter de abertura do
+      // topo da pilha. Se formarem um par (), [], {}, desempilhar e apagar o item do topo
       it = pilha_topo(pilha_aberta);
-      if (c == ')' && item_get_chave(it) == '(') {
-        it = pilha_desempilhar(pilha_aberta);
-        item_apagar(&it);
-      } else
-      if (c == ']' && item_get_chave(it) == '[') {
-        it = pilha_desempilhar(pilha_aberta);
-        item_apagar(&it);
-      } else
-      if (c == '}' && item_get_chave(it) == '{') {
+      if (
+        (c == ')' && item_get_chave(it) == '(') ||
+        (c == ']' && item_get_chave(it) == '[') ||
+        (c == '}' && item_get_chave(it) == '{') && 
+        pilha_tamanho(pilha_aberta) > 0
+      ) {
         it = pilha_desempilhar(pilha_aberta);
         item_apagar(&it);
       } else {
-        item = item_criar(c);
-        pilha_empilhar(pilha_aberta, item);
-        break;
+
+        // Caso nao seja um caracter de abertura e nao forme um par com o
+        // caracter do topo da pilha, finalizar o loop pois a sequencia
+        // ja esta desbalanceada
+        pilha_apagar(&pilha_aberta);
+        return false;
       }
     }
-    
     c = sequencia[i];
     i++;
   }
-  bool str_balanceada = true;
+
+  // Caso todos os caracteres tenham sido verificados
+  // verificamos tambem se a pilha foi completamente liberada (todos 
+  // os pares se cancelaram), e apresenta tamanho 0. Se nao, esta desbalanceada.
+  // Por fim liberamos a pilha da memoria.
   if (pilha_tamanho(pilha_aberta) != 0) str_balanceada = false;
   pilha_apagar(&pilha_aberta);
   return str_balanceada;
