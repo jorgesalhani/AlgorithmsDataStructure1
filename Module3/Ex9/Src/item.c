38480f73ef5ffad3093bdf791afc1c8a867cc891
jorgesalhani <jorge.salhani@usp.br>
2022-10-25 17:08:05 -0300

    Start functions .c

---
+++ Module3/Ex9/Src/item.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "item.h"
+
+struct item_ {
+  int chave;
+};
+
+ITEM *item_criar(int chave) {
+
+}
+
+bool item_apagar(ITEM **item) {
+
+}
+
+void item_imprimir(ITEM *item) {
+
+}
+
+int item_get_chave(ITEM *item) {
+
+}
+
+bool item_set_chave(ITEM *item, int chave) {
+
+}
---
+++ Module3/Ex9/Src/lista.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+typedef struct no_ NO; 
+
+struct no_{
+  ITEM *item;
+  NO *proximo;
+};
+
+struct lista_{ 
+  NO *inicio;
+  NO *fim;    
+  int tamanho;  /*tamanho da lista*/
+};
+
+LISTA *lista_criar(void) {
+
+}
+
+bool lista_inserir(LISTA *lista, ITEM *item) {
+
+}
+
+ITEM *lista_remover(LISTA *lista, int chave) {
+
+}
+
+bool lista_apagar(LISTA **lista) {
+
+}
+
+ITEM *lista_busca(LISTA *lista, int chave) {
+
+}
+
+int lista_tamanho(LISTA *lista) {
+
+}
+
+bool lista_vazia(LISTA *lista) {
+
+}
+
+bool lista_cheia(LISTA *lista) {
+
+}
+
+void lista_imprimir(LISTA *lista) {
+
+}
+
+
+bool lista_inverter(LISTA **lista) {
+
+}
+
