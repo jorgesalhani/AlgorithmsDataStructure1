7c8ba23cb8ce9e49abecd8bf8de7a4d4af7ae28f
jorgesalhani <jorge.salhani@usp.br>
2022-11-20 18:55:39 -0300

    Finish Ex10. No leaks. 3/3 Tests passed

--- Module4/Ex10/Src/main.c
+++ Module4/Ex10/Src/main.c
@@ -1,31 +1,23 @@
+/**
+ * @author Jorge Augusto Salgado Salhani
+ * @brief Criacao de ABB (Arvore Binaria de Busca) com operacoes
+ *        principais de: insercao, remocao e busca
+ * 
+ * Serao informados:
+ * - lista de valores para insercao na ABB
+ * - Sequencia de operacoes
+ *  1 - caso busca elemento i
+ *  2 - caso remocao do elemento i
+ * 
+ * O TAD ABB utiliza do TAD ITEM
+ * 
+*/
+
 #include "ABB.h"
 #include <stdio.h>
 #include <stdlib.h>
     
 int main(int argc, char *argv[]){
-
-  // ABB *T;
-  // T = abb_criar();
-  // ITEM* item1 = item_criar(2);
-  // ITEM* item2 = item_criar(1);
-  // ITEM* item3 = item_criar(3);
-  // ITEM* item4 = item_criar(5);
-  // ITEM* item5 = item_criar(7);
-  // ITEM* item6 = item_criar(6);
-  // ITEM* item7 = item_criar(4);
-  // abb_inserir(T, item1);
-  // abb_inserir(T, item2);
-  // abb_inserir(T, item3);
-  // abb_inserir(T, item4);
-  // abb_inserir(T, item5);
-  // abb_inserir(T, item6);
-  // abb_inserir(T, item7);
-  // abb_remover(T, 2);
-  // ITEM* it = abb_busca(T, 4);
-  // item_imprimir(it);
-  // abb_apagar(&T);
-
-
   ABB *T;
   int n, m, x, op;
   
